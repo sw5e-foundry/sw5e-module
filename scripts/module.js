72bdfd813d8e43001638d113de41d60183a1372a
Ikaguia <Ikaguia@users.noreply.github.com>
2024-07-20 14:25:24 -0300

    Source Packs

--- scripts/module.js
+++ scripts/module.js
@@ -1,9 +1,15 @@
 import { patchConfig } from "./config.mjs";
 
+const strict = true;
+
 Hooks.once('init', async function() {
-	patchConfig(CONFIG.DND5E);
+	patchConfig(CONFIG.DND5E, strict);
 });
 
 Hooks.once('ready', async function() {
-
+	if (strict) {
+		game.packs.filter(p => p.metadata.packageName === "dnd5e").forEach(p => {
+			foundry.utils.setProperty(p.metadata.flags, "dnd5e.types", ["nope"]);
+		});
+	}
 });
