1bc284a2f38bcbf7a24694f67fe9ff507a7b6277
Ikaguia <Ikaguia@users.noreply.github.com>
2024-07-22 16:00:28 -0300

    Properties: Keen

--- scripts/properties.mjs
+++ scripts/properties.mjs
@@ -1,3 +1,14 @@
+function patchKeen() {
+	function useKeen(wrapped, ...args) {
+		const keen = this.parent?.flags?.["sw5e-module-test"]?.properties?.keen ?? 0;
+		console.debug("keen", keen);
+		const result = wrapped(...args);
+		return result === Infinity ? Math.max(15, 20 - keen) : result;
+	}
+	libWrapper.register('sw5e-module-test', 'dnd5e.dataModels.item.WeaponData.prototype._typeCriticalThreshold', useKeen, 'MIXED' );
+	libWrapper.register('sw5e-module-test', 'dnd5e.dataModels.item.ConsumableData.prototype._typeCriticalThreshold', useKeen, 'MIXED' );
+}
+
 export function patchProperties() {
 	Hooks.on("renderItemSheet5e", (app, html, data) => {
 		for (const type of ["equipment", "weapon"]) {
@@ -63,4 +74,5 @@ export function patchProperties() {
 			});
 		}
 	});
+	patchKeen();
 }
