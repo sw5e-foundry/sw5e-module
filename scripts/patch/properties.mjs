d4424fed449526437a3fd4e00ff7924116290f1a
Ikaguia <Ikaguia@users.noreply.github.com>
2024-11-26 22:32:16 -0300

    Remove Reload + Fix Numeric Item Properties UI

--- scripts/patch/properties.mjs
+++ scripts/patch/properties.mjs
@@ -11,68 +11,87 @@ function addHelper() {
 
 function patchSheet() {
 	Hooks.on("renderItemSheet5e", (app, html, data) => {
-		for (const type of ["equipment", "weapon"]) {
-			const tag = `${type}-properties`;
-			html.find(`.${tag}`).each(async (idx, el) => {
-				const properties = new Set(Object.keys((await app.getData()).properties));
-				const numericProperties = new Set([...properties].filter(key => CONFIG.DND5E.itemProperties[key]?.type === "Number"));
-				const boolProperties = properties.difference(numericProperties);
-				const itemProperties = app.item.system.properties;
+		const tabDetails = html.find('.tab.details')[0];
+		const fieldset = tabDetails.firstElementChild;
+		const wpDiv = fieldset.lastElementChild;
+		const wpLabel = wpDiv.firstElementChild;
 
-				const boolNode = document.createElement("div");
-				boolNode.setAttribute("class", `form-group stacked ${tag}`);
-				boolNode.appendChild(el.firstElementChild.cloneNode(true));
-				for (const prop of boolProperties) {
-					const config = CONFIG.DND5E.itemProperties[prop];
-					const path = `system.properties.${prop}`;
-					const value = itemProperties.has(prop);
-					const labelNode = document.createElement("label");
-					labelNode.setAttribute("class", "checkbox");
-					const inputNode = document.createElement("input");
-					inputNode.setAttribute("type", "checkbox");
-					inputNode.setAttribute("name", path);
-					if (value) inputNode.setAttribute("checked", null);
-					labelNode.appendChild(inputNode);
-					const textNode = document.createTextNode(config.label);
-					labelNode.appendChild(textNode);
-					boolNode.appendChild(labelNode);
-				}
+		const properties = new Set(data.properties.options.map(p=>p.value));
+		const numericProperties = new Set([...properties].filter(key => CONFIG.DND5E.itemProperties[key]?.type === "Number"));
+		const boolProperties = properties.difference(numericProperties);
+		const itemProperties = app.item.system.properties;
 
-				const numericNode = document.createElement("div");
-				numericNode.setAttribute("class", `form-group grid ${tag}`);
-				for (const prop of numericProperties) {
-					const config = CONFIG.DND5E.itemProperties[prop];
-					const path = `flags.sw5e.properties.${prop}`;
-					const value = foundry.utils.getProperty(app.item, path) ?? config.default;
-					const labelNode = document.createElement("label");
-					labelNode.setAttribute("class", "number");
-					const textNode = document.createTextNode(config.label);
-					labelNode.appendChild(textNode);
-					const inputNode = document.createElement("input");
-					inputNode.setAttribute("type", "text");
-					inputNode.setAttribute("name", path);
-					inputNode.setAttribute("value", value ?? "");
-					inputNode.setAttribute("data-dtype", "Number");
-					inputNode.addEventListener("input", () => {
-						const oldVal = itemProperties.has(prop);
-						const newVal = inputNode.value !== "";
-						if (oldVal !== newVal) {
-							if (oldVal) itemProperties.delete(prop);
-							else itemProperties.add(prop);
-							app.item.update({ "system.properties": Array.from(itemProperties) }, { recursive: false });
-							app.item.update({[path]: newVal ? inputNode.value : null});
-						}
-					});
-					labelNode.appendChild(inputNode);
-					numericNode.appendChild(labelNode);
-				}
+		const boolNode = document.createElement("div");
+		{
+			boolNode.setAttribute("class", `form-group stacked checkbox-grid`);
+			boolNode.appendChild(wpDiv.firstElementChild.cloneNode(true));
+			const formFields = document.createElement("div");
+			formFields.setAttribute("class", `form-fields`);
+			boolNode.appendChild(formFields);
+			for (const prop of boolProperties) {
+				const config = CONFIG.DND5E.itemProperties[prop];
+				const path = `system.properties.${prop}`;
+				const value = itemProperties.has(prop);
 
-				const parentNode = el.parentNode;
-				parentNode.insertBefore(boolNode, el);
-				parentNode.insertBefore(numericNode, el);
-				el.remove();
-			});
+				const labelNode = document.createElement("label");
+				labelNode.setAttribute("class", "checkbox");
+
+				const inputNode = document.createElement("dnd5e-checkbox");
+				inputNode.setAttribute("name", path);
+				inputNode.setAttribute("tabindex", 0);
+				if (value) inputNode.setAttribute("checked", null);
+				labelNode.appendChild(inputNode);
+
+				const spanNode = document.createElement("span");
+				const textNode = document.createTextNode(config.label);
+				spanNode.appendChild(textNode);
+				labelNode.appendChild(spanNode);
+				formFields.appendChild(labelNode);
+			}
 		}
+
+		const numericNode = document.createElement("div");
+		{
+			numericNode.setAttribute("class", `form-group grid checkbox-grid`);
+			const formFields = document.createElement("div");
+			formFields.setAttribute("class", `form-fields`);
+			numericNode.appendChild(formFields);
+			for (const prop of numericProperties) {
+				const config = CONFIG.DND5E.itemProperties[prop];
+				const path = `flags.sw5e.properties.${prop}`;
+				const value = foundry.utils.getProperty(app.item, path) ?? config.default;
+
+				const labelNode = document.createElement("label");
+				labelNode.setAttribute("class", "number");
+
+				const spanNode = document.createElement("span");
+				const textNode = document.createTextNode(config.label);
+				spanNode.appendChild(textNode);
+				labelNode.appendChild(textNode);
+
+				const inputNode = document.createElement("input");
+				inputNode.setAttribute("type", "text");
+				inputNode.setAttribute("name", path);
+				inputNode.setAttribute("value", value ?? "");
+				inputNode.setAttribute("data-dtype", "Number");
+				inputNode.addEventListener("input", () => {
+					const oldVal = itemProperties.has(prop);
+					const newVal = inputNode.value !== "";
+					if (oldVal !== newVal) {
+						if (oldVal) itemProperties.delete(prop);
+						else itemProperties.add(prop);
+						app.item.update({ "system.properties": Array.from(itemProperties) }, { recursive: false });
+						app.item.update({[path]: newVal ? inputNode.value : null});
+					}
+				});
+				labelNode.appendChild(inputNode);
+				formFields.appendChild(labelNode);
+			}
+		}
+
+		fieldset.insertBefore(boolNode, wpDiv);
+		fieldset.insertBefore(numericNode, wpDiv);
+		wpDiv.remove();
 	});
 }
 
@@ -80,6 +99,6 @@ export function patchProperties() {
 	addHelper();
 	patchSheet();
 
-	patchReload();
+	// patchReload();
 	patchKeen();
 }
