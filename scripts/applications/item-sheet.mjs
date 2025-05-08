f894b943fac959b1dcee08b14bbc37dd71ace20d
CK <31608392+unrealkakeman89@users.noreply.github.com>
2025-05-08 12:00:41 -0400

    Update item-sheet.mjs

--- scripts/applications/item-sheet.mjs
+++ scripts/applications/item-sheet.mjs
@@ -1,6 +1,27 @@
+import { reloadBlaster } from './blaster-reload.mjs';
+
 export class ItemSheetSW5E extends globalThis.dnd5e.applications.item.ItemSheet5e {
 	/** @inheritdoc */
 	get template() {
 		return `modules/sw5e/templates/items/${this.item.type.substring(5)}.hbs`;
 	}
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Handle the reload blaster button click
+    html.on('click', '.reload-blaster', this._onReloadBlaster.bind(this));
+  }
+
+  async _onReloadBlaster(event) {
+    event.preventDefault();
+    const itemId = event.currentTarget.dataset.itemId;
+    const item = this.item; // 'this.item' refers to the Item being displayed in the sheet
+    const actor = item.parent; // Get the Actor who owns the item
+
+    if (item?.type === 'weapon' && ['simpleBL', 'martialBL', 'exoticBL'].includes(item?.data?.data?.weaponType)) {
+      reloadBlaster(actor, item);
+    }
+  }
 }
